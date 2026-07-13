6af09e4e911cf8a22e8028d5205b2cbf1ef6a54b
JustAUser <timofeyaxenov98@gmail.com>
2026-07-13 21:33:49 +0300

    Hopefully a working version

--- obj/Release/net462/OneShot.Archipelago.AssemblyInfo.cs
+++ obj/Release/net462/OneShot.Archipelago.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("oneshot.archipelago")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a694a9aa562e04f6614df0c0d917b13c8791a77")]
 [assembly: System.Reflection.AssemblyProductAttribute("oneshot.archipelago")]
